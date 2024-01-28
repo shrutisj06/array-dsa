b79010ead902cf0c09a6c98ec8dbf0d4eaee452c
shrutisj06 <157938791+shrutisj06@users.noreply.github.com>
2024-01-28 13:15:16 +0530

    Add files via upload

---
+++ selection sort.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void selection_sort(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        int min=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if (arr[j]<arr[min])
+               {
+
+            int temp=arr[min];
+            arr [min]=arr[j];
+            arr[j]=temp;
+               }
+        }
+    }
+
+}
+
+void main()
+{
+    int arr[]={5,6,8,9,7};
+    int n=5;
+    selection_sort(arr,n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
